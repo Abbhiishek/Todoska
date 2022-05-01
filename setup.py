c08f5f595a975922f51eb7861035d3a960725ffa
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-05-01 09:15:26 +0530

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
- 
+import setuptools
+
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
@@ -10,15 +11,20 @@ classifiers = [
  
 setup(
   name='Todoska',
-  version='0.1.01',
+  version='0.2.06',
   description='A Basic Todo Tracker in Cli',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='https://github.com/Abbhiishek/Todoska',  
   author='Abhishek Kushwaha',
-  author_email='josh@edublocks.org',
+  author_email='abhishekkushwaha1479@gmail.com',
   license='MIT', 
+  entry_points='''
+        [console_scripts]
+        todoska=Todo-Tracker.__main__:app
+    ''',
   classifiers=classifiers,
-  keywords='Python , cli , Todo ,Todotracker , todo-tracker', 
-  packages=find_packages(),
-  install_requires=['typer' , 'rich' , 'pysqlite3'] 
+  keywords=['Python' , 'cli' , 'Todo' ,'Todotracker' , 'todo-tracker' , 'todo-tracker-cli', 'Todoska'], 
+  packages=setuptools.find_packages(),
+  python_requires='>=3.6',
+  install_requires=['typer' , 'rich' ] 
 )
